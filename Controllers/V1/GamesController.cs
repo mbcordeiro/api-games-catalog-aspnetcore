6c3c1b6e5b432f311676c853f102d79480204588
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-06-21 19:30:28 -0300

    Game service interface and di game controller

--- Controllers/V1/GamesController.cs
+++ Controllers/V1/GamesController.cs
@@ -1,9 +1,11 @@
 ﻿using ApiGamesCatalog.InputModels;
+using ApiGamesCatalog.Services;
 using ApiGamesCatalog.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,40 +15,87 @@ namespace ApiGamesCatalog.Controllers.V1
     [ApiController]
     public class GamesController : ControllerBase
     {
-        [HttpGet]
-        public async Task<ActionResult<List<GameViewModel>>> GetGames()
+        private readonly IGameService _gameService;
+
+        public GamesController(IGameService gameService)
         {
-            return Ok();
+            _gameService = gameService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<GameViewModel>>> GetGames([FromQuery, Range(1, int.MaxValue)] int page = 1, [FromQuery, Range(1, 50)] int quantity = 5)
+        {
+            var games = await _gameService.GetGames(1, 10);
+            if(games.Count() == 0)
+                return NoContent();
+            
+            return Ok(games);
         }
 
         [HttpGet("id: guild")]
-        public async Task<ActionResult<List<GameViewModel>>> GetGameById(Guid id)
+        public async Task<ActionResult<List<GameViewModel>>> GetGameById([FromRoute]Guid id)
         {
+            var game = await _gameService.GetGameById(id);
+            if (game == null)
+                return NoContent();
+
             return Ok();
         }
 
         [HttpPost]
-        public async Task<ActionResult<List<GameViewModel>>> InsertGame(GameInputModel game)
+        public async Task<ActionResult<List<GameViewModel>>> InsertGame([FromBody] GameInputModel gameInputModel)
         {
-            return Created("", game);
+            try
+            {
+                var game = await _gameService.InsertGame(gameInputModel);
+                return Created("", null);
+            }
+            catch(Exception ex)
+            {
+                return UnprocessableEntity("Error");
+            }
         }
 
         [HttpPut("id: guild")]
-        public async Task<ActionResult<List<GameViewModel>>> UpdateGame(Guid id, GameInputModel game)
+        public async Task<ActionResult<List<GameViewModel>>> UpdateGame([FromRoute] Guid id, [FromBody] GameInputModel game)
         {
-            return NoContent();
+            try
+            {
+                await _gameService.UpdateGame(id, game);
+                return NoContent(); 
+            } 
+            catch (Exception ex)
+            {
+                return NotFound("Game not found");
+            }
         }
 
         [HttpPatch("id: guild/price/{price:double}")]
-        public async Task<ActionResult<List<GameViewModel>>> UpdateGamePrice(Guid id, double preco)
+        public async Task<ActionResult<List<GameViewModel>>> UpdateGamePrice([FromRoute] Guid id, [FromRoute] double price)
         {
-            return NoContent();
+            try
+            {
+                await _gameService.UpdateGamePrice(id, price);
+                return NoContent();
+            }
+            catch (Exception ex) 
+            {
+                return NotFound("Game not found");
+            }
         }
 
         [HttpDelete("id: guild")]
         public async Task<ActionResult<List<GameViewModel>>> DeleteGame(Guid id)
         {
-            return NoContent();
+            try
+            {
+                await _gameService.DeleteGame(id);
+                return NoContent();
+            }
+            catch
+            {
+                return NotFound("Game not found");
+            }
         }
     }
 }
---
+++ Services/IGameService.cs
@@ -0,0 +1,19 @@
+﻿using ApiGamesCatalog.InputModels;
+using ApiGamesCatalog.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiGamesCatalog.Services
+{ 
+    public interface IGameService : IDisposable
+    {
+        Task<List<GameViewModel>> GetGames(int page, int quantity);
+        Task<GameViewModel> GetGameById(Guid id);
+        Task<GameViewModel> InsertGame(GameInputModel game);
+        Task UpdateGame(Guid id, GameInputModel game);
+        Task UpdateGamePrice(Guid id, double price);
+        Task DeleteGame(Guid id);
+    }
+}
